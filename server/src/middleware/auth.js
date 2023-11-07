318bd1cad81c3ade0825f24a97113f85fd5a7c93
Ahni Anderson <ahnianderson@Ahnis-MacBook-Pro.local>
2023-11-07 12:01:05 -0500

    shgsif

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,23 +1,29 @@
+// server/src/middleware/auth.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-const protect = async (req, res, next) => {
-    let token;
+exports.protect = async (req, res, next) => {
+  let token;
 
-    if (req.headers.authoriztion && req.headers.authorization.startsWith('Bearer')) {
-        try {
-            token = req.headers.authorization.split(' ')[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select('-password');
-            next();
-        } catch (error) {
-            console.error(error);
-            res.status(401).json({ message: 'Not authorized, token failed' });
-        }
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: 'Not authorized' });
     }
-    if (!token) {
-        res.status(401).json({ message: 'Note authorized, no token' });
-    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
 };
-
-module.exports = { protect };
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,24 +1,25 @@
 // server/src/routes/userRoutes.js
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 // Helper function to generate JWT
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+const generateToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, {
     expiresIn: '30d', // Token valid for 30 days
   });
 };
 
 // Register user
 router.post('/register', async (req, res) => {
-  const { username, password, role } = req.body;
+  const { username, email, password, role } = req.body;
 
   // Validate request body
-  if (!username || !password || !role) {
+  if (!username || !email || !password || !role) {
     return res.status(400).json({ message: 'Please add all fields' });
   }
 
@@ -28,15 +29,20 @@ router.post('/register', async (req, res) => {
   }
 
   // Check if user already exists
-  const userExists = await User.findOne({ username });
+  const userExists = await User.findOne({ email });
   if (userExists) {
     return res.status(400).json({ message: 'User already exists' });
   }
 
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   // Create user
   const user = await User.create({
     username,
-    password,
+    email,
+    password: hashedPassword,
     role,
   });
 
@@ -44,8 +50,9 @@ router.post('/register', async (req, res) => {
     res.status(201).json({
       _id: user.id,
       username: user.username,
+      email: user.email,
       role: user.role,
-      token: generateToken(user._id),
+      token: generateToken(user._id, user.role),
     });
   } else {
     res.status(400).json({ message: 'Invalid user data' });
@@ -54,17 +61,18 @@ router.post('/register', async (req, res) => {
 
 // Login user
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
   // Check for user email
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ email });
 
-  if (user && (await user.comparePassword(password))) {
+  if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
       _id: user.id,
       username: user.username,
+      email: user.email,
       role: user.role,
-      token: generateToken(user._id),
+      token: generateToken(user._id, user.role),
     });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
@@ -73,12 +81,13 @@ router.post('/login', async (req, res) => {
 
 // Get user profile
 router.get('/profile', protect, async (req, res) => {
-  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user.id);
 
   if (user) {
     res.json({
       _id: user.id,
       username: user.username,
+      email: user.email,
       role: user.role,
     });
   } else {
