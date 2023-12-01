57d6c25f623ee3b3c490bcf6882142631de4aef1
Ahni Anderson <ahnianderson@Ahnis-MBP.attlocal.net>
2023-12-01 17:49:55 -0500

    sgs

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Sidebar.css'; // Adjust the path as necessary
+import './Sidebar.css';
 
 const Sidebar = () => {
-    // Check if the user is logged in
-  const isLoggedIn = localStorage.getItem('userToken') !== null;
+  const navigate = useNavigate();
+  const isLoggedIn = localStorage.getItem('user') !== null;
 
   const handleLogout = () => {
-    // Logic to handle logout
-    localStorage.removeItem('userToken');
-    // Redirect to home page
-    navigate('/');
+    localStorage.removeItem('user');
+    navigate('/login'); // Redirect to login page after logout
   };
 
   return (
@@ -22,17 +20,16 @@ const Sidebar = () => {
               Home
             </Link>
           </li>
-
           <li className="nav-item">
             <Link className="nav-link" to="/register">
               Register
             </Link>
           </li>
           <li className="nav-item">
-          {isLoggedIn ? (
-              <Link className="nav-link" to="/" onClick={handleLogout}>
+            {isLoggedIn ? (
+              <button className="nav-link" onClick={handleLogout}>
                 Logout
-              </Link>
+              </button>
             ) : (
               <Link className="nav-link" to="/login">
                 Login
