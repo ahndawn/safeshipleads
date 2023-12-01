7541f90ca10570c92f5c071a0ce0a1409106df31
Ahni Anderson <ahnianderson@Ahnis-MBP.attlocal.net>
2023-12-01 17:09:39 -0500

    sgs

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,16 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Register from './components/users/Register';
-import userAuth from './services/userAuth/userAuth';
+import userAuth from './services/userAuth';
 import Login from './components/users/Login';
 import Profile from './components/users/Profile';
 import AdminHome from './components/homepage/AdminHome';
 import VendorHome from './components/homepage/VendorHome';
 
 const App = () => {
-  const isAuthenticated = userAuth();
-
-  // Logic to determine user role (placeholder)
-  const userRole = 'admin'; // This should be dynamically determined
+  const isAuthenticated = userAuth(); 
+  // Define userRole here. This is just an example. Adjust according to your logic.
+  const userRole = isAuthenticated ? JSON.parse(localStorage.getItem('user')).role : null;
 
   const renderHomePage = () => {
     switch (userRole) {
@@ -33,10 +32,10 @@ const App = () => {
           <Sidebar />
           <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4 main-content">
             <Routes>
-              <Route path="/" element={isAuthenticated() ? renderHomePage() : <Navigate to="/login" />} />
+              <Route path="/" element={isAuthenticated ? renderHomePage() : <Navigate to="/login" />} />
               <Route path="/register" element={<Register />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/profile" element={isAuthenticated() ? <Profile /> : <Navigate to="/login" />} />
+              <Route path="/profile" element={isAuthenticated ? <Profile /> : <Navigate to="/login" />} />
             </Routes>
           </main>
         </div>
